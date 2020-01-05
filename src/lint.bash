47465ac08e59c280d012e8bc4968ef6ffa048f96
Chris <7539871+paxperscientiam@users.noreply.github.com>
2020-01-05 18:39:31 -0500

    first

---
+++ src/lint.bash
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+lint::color:warn() {
+    tput setaf 3 # yellow
+    printf '%s\n' "${1}"
+    tput sgr0
+}
+lint::color:error() {
+    tput setaf 1 # red
+    printf '%s\n' "${1}"
+    tput sgr0
+}
+lint::color:notice() {
+    tput setaf 6 # cyan
+    printf '%s\n' "${1}"
+    tput sgr0
+}
+
+lint::table() {
+    local input="${1?}"
+    local ts="tslint"
+    local json="jsonlint"
+    lint::color:notice "using ${!1} ..." >/dev/tty
+    echo "${!1}"
+}
+
+lint() {
+    local file
+    file="$(basename "$1")"
+    local ext
+    ext="${file##*.}"
+    local linter
+    linter=$(lint::table "$ext")
+    $linter "$*"
+}
+
+if [[ ${BASH_SOURCE[0]} != "$0" ]]; then
+    export -f lint
+else
+    lint "${@}"
+    exit $?
+fi
